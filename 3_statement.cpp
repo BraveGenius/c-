9311e7fcb43413d67b6af87c544f712e2701f9bc
Ankit Yadav <137118259+BraveGenius@users.noreply.github.com>
2023-10-02 02:42:03 +0530

    Add files via upload

---
+++ 3_statement.cpp
@@ -0,0 +1,171 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int pocketmoney=3000;
+//     for(int date=1;date<=30;date++){
+//         if(date%2==0){
+//             continue;
+//         }
+//         if (pocketmoney==0){
+//             break;
+//         }
+//         cout<<"go out today"<<endl;
+//         pocketmoney=pocketmoney-300;
+//     }
+//     return 0;
+// }
+//////////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     for(int i=1; i<=100;i++){
+//         if(i%3==0){
+//             continue;
+//         }
+//         cout<<i<<endl;
+//     }
+
+// }
+//////////////////////////////////////////////////////
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter the number :";
+//     cin>>n;
+//     int i;
+//     for(int i=2;i<n;i++){
+//         if(n%i==0){
+//             cout<<" non prime"<<endl;
+//             break;
+//         }
+//     }
+   
+//      if(i==n){
+//     cout<<"prime"<<endl;
+//     }
+//     return 0;
+    
+// }
+/////////////////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cout<<"Enter the two number :";
+//     cin>>a>>b;
+//     for(int num=a;num<=b;num++){
+//         int i;
+//         for(i=2;i<num;i++){
+//             if(num%i==0){
+//                 break;
+//             }
+           
+//             }
+//              if(i==num){
+//                 cout<<num<<endl;
+//         }
+//     }
+//     return 0;
+// }
+//////////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+// int main(){
+
+// char button;
+// cout<<"input a charecter :";
+// cin>>button;
+// if (button=='a')
+// {
+//     cout<<"hello"<<endl;
+// }
+// else if(button=='b'){
+//     cout<<"namaste"<<endl;
+// }
+// else if(button=='c'){
+//     cout<<"salut"<<endl;
+// }
+// else if(button=='d'){
+//     cout<<"hola"<<endl;
+// }
+// else if(button=='e'){
+//     cout<<"ciao"<<endl;
+// }
+// else {
+//     cout<<"i am still learnimg"<<endl;
+// }
+
+//     return 0;
+// }
+/////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char button;
+// cout<<"input a charecter :";
+// cin>>button;
+// switch (button)
+
+// {
+// case 'a':
+//     cout<<"hello"<<endl;/* code */
+//     break;
+// case 'b':
+// cout<<"namaste"<<endl;
+// break;
+// case 'c':
+//     cout<<"anna"<<endl;/* code */
+//     break;
+//     case 'd':
+//     cout<<"elle"<<endl;/* code */
+//     break;
+//     case 'e':
+//     cout<<"salut"<<endl;/* code */
+//     break;
+//     case 'f':
+//     cout<<"hola"<<endl;/* code */
+//     break;
+//     case 'g':
+//     cout<<"ciao"<<endl;/* code */
+//     break;
+
+// default:
+// cout<<"i am still learning "<<endl;
+//     break;
+// } 
+//     return 0;
+// }
+////////////////////////////////////////////////
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n1,n2;
+//     cout<<"enter the 2 number :";
+//     cin>>n1>>n2;
+// char op;
+// cout<<"input the operator";
+// cin>>op;
+
+// switch (op)
+// {
+// case '+'/* constant-expression */:
+//     cout<<n1+n2<<endl;         /* code */
+//     break;
+//     case '-':
+//     cout<<n1-n2<<endl;         /* code */
+//     break;
+//     case '*':
+//     cout<<n1*n2<<endl;         /* code */
+//     break;
+//     case '%':
+//     cout<<n1%n2<<endl;         /* code */
+//     break;
+
+// default:
+//     cout<<"enter the valid operator:";
+//     break;
+// }   
+//     return 0;
+// }
